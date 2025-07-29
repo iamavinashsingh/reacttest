ff3f739dbf72e80c3f9de19f9dca5803d4e602a5
Avinash Singh <imavinashsingh2@gmail.com>
2025-07-29 23:12:49 +0530

    Bottom Tab 2 Navigation

--- App.jsx
+++ App.jsx
@@ -4,45 +4,64 @@ import Explore from './src/screens/Explore';
 import Profile from './src/screens/Profile';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Entypo';
 
 const Tab = createBottomTabNavigator();
 
-const StackNavigator = () => {
-  return (
-    <Stack.Navigator initialRouteName="Home"
-      screenOptions={{
-        headerStyle: { backgroundColor: 'blue' },
-        headerTintColor: '#fff',
-        headerTitleStyle: { fontWeight: 'bold' },
-        headerTitleAlign: 'center',
-        headerBackVisible: false,
-      }}
-    > 
-      <Stack.Screen 
-        name="Home" 
-        component={Home}  
+// const StackNavigator = () => {
+//   return (
+//     <Stack.Navigator initialRouteName="Home"
+//       screenOptions={{
+//         headerStyle: { backgroundColor: 'blue' },
+//         headerTintColor: '#fff',
+//         headerTitleStyle: { fontWeight: 'bold' },
+//         headerTitleAlign: 'center',
+//         headerBackVisible: false,
+//       }}
+//     > 
+//       <Stack.Screen 
+//         name="Home" 
+//         component={Home}  
         
-      />
-      <Stack.Screen 
-        name="Explore" 
-        component={Explore} 
+//       />
+//       <Stack.Screen 
+//         name="Explore" 
+//         component={Explore} 
         
-      />
-      <Stack.Screen 
-        name="Profile" 
-        component={Profile} 
+//       />
+//       <Stack.Screen 
+//         name="Profile" 
+//         component={Profile} 
         
-      />
-    </Stack.Navigator>
-  );
-};
+//       />
+//     </Stack.Navigator>
+//   );
+// };
 
 const TabNavigator = () =>{
-  <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Explore" component={Explore} />
-      <Tab.Screen name="Profile" component={Profile} />
+  return (
+    <Tab.Navigator 
+    screenOptions={{headerShown: false}}>
+      <Tab.Screen   
+        name="Home" 
+        component={Home} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home" size={25} color='red' />
+          )
+        }}
+      />
+      <Tab.Screen 
+        name="Explore" 
+        component={Explore} 
+      />
+      <Tab.Screen   
+        name="Profile" 
+        component={Profile}
+      />
     </Tab.Navigator>
+  );
+  
 }
 
 const App = () => {
